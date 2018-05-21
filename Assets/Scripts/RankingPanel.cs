31a118b7b6e706930e0a9310953a5597de235064
Pawat Limpanuthai <pawat@digitopolisstudio.com>
2018-05-21 19:34:01 +0700

    add animation fade in on ranking panel

--- Assets/Scripts/RankingPanel.cs
+++ Assets/Scripts/RankingPanel.cs
@@ -1,13 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RankingPanel : MonoBehaviour {
+	public Button screenBtn;
 
 	public List<RankingCell> cellList;
+
+	void OnEnable()
+	{
+        screenBtn.gameObject.SetActive(true);
+		cellList.ForEach(list => list.gameObject.SetActive(false));
+        
+		screenBtn.onClick.RemoveAllListeners();
+		screenBtn.onClick.AddListener(() => {
+			cellList[3].gameObject.SetActive(true);
+			screenBtn.onClick.RemoveAllListeners();
+
+            screenBtn.onClick.AddListener(() => {
+                cellList[2].gameObject.SetActive(true);
+                screenBtn.onClick.RemoveAllListeners();
+
+                screenBtn.onClick.AddListener(() => {
+                    cellList[1].gameObject.SetActive(true);
+                    cellList[0].gameObject.SetActive(true);
+                    screenBtn.onClick.RemoveAllListeners();
+                    screenBtn.gameObject.SetActive(false);
+				});
+			});
+		});
+	}
 
 	public void ResetGame()
 	{
 		
 	}
+
+
 }
